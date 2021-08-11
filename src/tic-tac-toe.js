3d45a88952773c58f00f618cbc76edd2e958e18b
mitjasha <romantus_@mail.ru>
2021-08-11 17:23:40 +0300

    feat: add TicTacToe class

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,66 @@
 class TicTacToe {
     constructor() {
+        
+        this.board = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null],];
+        this.currentPlayerSymbol = 'o';
 
     }
 
     getCurrentPlayerSymbol() {
 
+        return (this.currentPlayerSymbol === 'x') ? 'o' : 'x';
     }
 
     nextTurn(rowIndex, columnIndex) {
 
+        if (this.board[rowIndex][columnIndex] === null){
+            this.board[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
+            this.currentPlayerSymbol = this.getCurrentPlayerSymbol();
+        }
     }
 
-    isFinished() {
+    isFinished(){
 
+        return this.isDraw() || this.getWinner() !== null;
     }
 
     getWinner() {
 
+        for (let i = 0; i < 3; i++){
+            if (this.board[i][0] === this.board[i][1] && this.board[i][0] === this.board[i][2]){
+                return this.board[i][0];
+
+            } else if (this.board[0][i] === this.board[1][i] && this.board[0][i] === this.board[2][i]){
+                return this.board[0][i];
+            }
+        }
+
+        if (this.board[0][0] === this.board[1][1] && this.board[0][0] === this.board[2][2]){
+            return this.board[1][1];
+
+        } else if (this.board[0][2] === this.board[1][1] && this.board[2][0] === this.board[1][1]){
+            return this.board[1][1];
+        }
+
+        return null;
     }
 
     noMoreTurns() {
 
+        return this.board.flat().includes(null) === false;
     }
 
     isDraw() {
 
+        return this.getWinner() === null && this.noMoreTurns();
     }
 
     getFieldValue(rowIndex, colIndex) {
 
+        return this.board[rowIndex][colIndex];        
     }
 }
 
